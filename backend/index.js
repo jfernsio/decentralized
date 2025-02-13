a4d4d63f5a4d6619e05af3df0487835006ef71c5
joel <vashappnin107@gmail.com>
2025-02-14 00:24:35 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,63 @@
 import express from "express";
 import connectDB from './config/connectDb.js'
+import cors from "cors";
 import userRouter from "./src/routes/user.js";
 import userrouter from "./src/routes/userRoutes.js";
 import workerRouter from "./src/routes/workerRoutes.js";
-import { Submission } from "./models/taskModel.js";
-import { authMiddleware } from "./authMiddelweare.js";
+import cookieParser from "cookie-parser";
+import { Option, Submission ,Task } from "./models/taskModel.js";
+import { authWorkerMiddleware ,authMiddleware } from "./authMiddelweare.js";
+import { generateSignature } from "./controllers/cloudinaryController.js";
 const app = express();
 app.use(express.json());
 
 connectDB()
+app.use(cors());
 app.use("/api/user", userrouter);
 app.use("/api/worker", workerRouter);
-app.post("/submit", authMiddleware, async (req, res) => {
+app.get('/api/get/signature',authMiddleware,generateSignature)
+app.post("/submit", authWorkerMiddleware, async (req, res) => {
   try {
     const { taskId, optionId } = req.body;
 
     const submission = await Submission.create({
       task_id: taskId,
       option_id: optionId,
-      // worker_id: '67a182ed99f3fb',
-      user_id: req.userId,
+      worker_id: req.userId,
     });
 
-    res.json({ success: true, submission });
+    const updateOption = await Option.findOneAndUpdate({
+      _id: optionId,
+      task_id: taskId,
+      done:true
+    }, {
+      $inc: { submissions: 1 }
+    })
+
+    res.json({ success: true, submission, updateOption});
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.delete("/api/delete/all/tasks",async(req,res)=>{
+  try {
+   const deleted =  await Task.deleteMany();
+    res.json({ success: true, count: deleted.deletedCount });
+    console.log(deleted.deletedCount);
+
+  } catch(e) {
+    return res.status(500).json({ message: e.message });
+  }    
+
+})
+app.get('/get/tasks',async(req,res)=>{
+  try {
+    const tasks = await Task.find()
+    res.json(tasks);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+      }
+      })
+app.listen(8000, () => {
+  console.log("Server is running on port 8000");
 });
