78a24895bbddbc5465371aabcbd7728a92588bce
joel <vashappnin107@gmail.com>
2025-02-14 00:30:14 +0530

    added frontend

---
+++ user-frontend/decentralized-frontend/src/app/components/AppBar.tsx
@@ -0,0 +1,14 @@
+export const AppBar = () => {
+    return (
+        <div className="flex justify-between border-b pb-2 pt-2">
+        <div className="text-2xl pl-4 flex justify-center pt-3">
+            Turkify
+        </div>
+        <div className="text-xl pr-4 pb-2">
+           
+        </div>
+    </div>
+
+    );
+}
+
